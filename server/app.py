750d58ce23e8d00ed9ea50e984d61f437cfa0ec6
thomas-waite <thomas.waite4@gmail.com>
2020-12-01 23:49:36 +0000

    refactor: get server working rename docker file to match tag

--- server/app.py
+++ server/app.py
@@ -1,28 +1,37 @@
 import json
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from model.Transformer import Model
+from flask_cors import CORS
 
 # Initialise
 app = Flask(__name__)
+CORS(app)
 model = Model()
 
 
 @app.route('/')
 def hello():
-    return json.dumps({'status': 200, 'body': 'Hello world'})
+    return json.dumps({'statusCode': 200, 'body': 'Hello world'})
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    input_text = request.form['text']
+    # TODO: Flask is not depending 'Content-Type': 'application/json' correctly
+    # so having to force it to retrieve json
+    data = request.get_json(force=True)
+    {"text": "the football stadium is"}
+    input_text = data['text']
     generated_text = model.text_generation(input_text)
     message = json.dumps(generated_text)
 
-    return json.dumps({'status': 200, 'headers': {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    },
-        'body': message})
+    return jsonify(
+        {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                # 'Access-Control-Allow-Origin': '*'
+            },
+            'body': message})
 
 
 if __name__ == '__main__':
